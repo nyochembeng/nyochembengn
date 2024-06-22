d0cfa3d26f66c38a76ca754ad2f1b30c057441ba
Nyochembeng Enzo Nkengafac <nyochembengn@gmail.com>
2024-06-22 17:44:04 +0100

    changing PC

--- src/api/addProject.ts
+++ src/api/addProject.ts
@@ -1,14 +1,14 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-import clientPromise from '../../lib/mongodb';
+import { NextApiRequest, NextApiResponse } from "next";
+import clientPromise from "../../lib/mongodb";
 
-export default async (req: NextApiRequest, res: NextApiResponse) => {
-  if (req.method === 'POST') {
+const AddProject = async (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.method === "POST") {
     const client = await clientPromise;
-    const db = client.db('portfolio');
+    const db = client.db("portfolio");
 
     const { name, description } = req.body;
 
-    const project = await db.collection('projects').insertOne({
+    const project = await db.collection("projects").insertOne({
       name,
       description,
     });
@@ -18,3 +18,5 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     res.status(405).end();
   }
 };
+
+export default AddProject;
